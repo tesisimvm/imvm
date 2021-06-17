3248b6f2ee5eff983ce6f8b004bcd57aaac3c8a8
NicolasCervera <nicodapa@live.com>
2021-06-17 09:21:27 -0300

    Inicio Sesion Finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,14 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { LoginComponent } from './login/login.component';
+import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainNavComponent,
-    LoginComponent
+    LoginComponent,
+    InicioSesionComponent
   ],
   imports: [
     BrowserModule,
