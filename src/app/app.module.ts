09e901d9ba5ba6513ba9849f9b01f32bfc3d1505
Dandrea Omar <omar_dandrea97@hotmail.com>
2021-06-19 01:42:09 -0300

    se creo el componente perfil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatListModule } from '@angular/material/list';
 import { LoginComponent } from './login/login.component';
 import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
 import { PagNosotrosComponent } from './nosotros/pag-nosotros/pag-nosotros.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { PagNosotrosComponent } from './nosotros/pag-nosotros/pag-nosotros.compo
     MainNavComponent,
     LoginComponent,
     InicioSesionComponent,
-    PagNosotrosComponent
+    PagNosotrosComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
