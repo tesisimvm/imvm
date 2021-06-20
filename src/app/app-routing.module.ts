97e49b9d562bf0f6a4b9c306f17ef4444ac81f62
Dandrea Omar <omar_dandrea97@hotmail.com>
2021-06-20 01:54:38 -0300

    enrutamiento solucionado y menu solucionado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
+import { LoginComponent } from './login/login.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { PagNosotrosComponent } from './nosotros/pag-nosotros/pag-nosotros.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: InicioSesionComponent}, /* cuando se inicia el proyecto o cuando la url esta vacia */
+  {path: 'inicio-sesion', component: InicioSesionComponent}, /* cuando te logueas para entrar a tu usuario */
+  {path: 'login', component: LoginComponent}, /* para registrate */
+  {path: 'nosotros', component: PagNosotrosComponent},
+  {path: 'perfil', component: PerfilComponent},
+  {path: 'main-nav', component: MainNavComponent, children:[
+
+    {path: 'perfil', component: PerfilComponent}
+  ]}, /* menu principal */
+  {path: '**', component: InicioSesionComponent}, /* por defecto - cuando hay problemas o se escribe mal la url */
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
