364f65dca27f3c01817459074cabecfd75745fe7
Dandrea Omar <omar_dandrea97@hotmail.com>
2021-08-19 08:33:17 -0300

    rediseño de la barra de navegacion

--- src/app/Views/Sesion/login/login.component.ts
+++ src/app/Views/Sesion/login/login.component.ts
@@ -7,8 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  /* public formGroup: FormGroup = new FormGroup({
+    nombreU: new FormControl(''),
+    apellidoU: new FormControl(''),
+    correo: new FormControl(''),
+    contraseniaU: new FormControl(''),
+  }); */
+
+  /* usu: any;
+
+  usuariArray: producto[] = []; */
+  
   constructor() { }
+
+
 
   ngOnInit(): void {
   }
+
+  /* se establece la llamada Post para crear un usuario - registrar usuario */
+
+  registrarUsuario(){
+    //variable que va a tener todos los datos capturados del usuario
+    
+    var RegistroU ={
+
+    }
+  }
+
 }
