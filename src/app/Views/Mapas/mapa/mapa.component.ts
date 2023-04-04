579717d311dd9ba5b6776108ab226fc248cbec10
Dandrea Omar <omar_dandrea97@hotmail.com>
2023-04-04 18:16:16 -0300

    servicio geolocalizacion y visualizacion de las coordenadas

--- src/app/Views/Mapas/mapa/mapa.component.ts
+++ src/app/Views/Mapas/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GeolocalizacionService } from 'src/app/service/geolocalizacion/geolocalizacion.service';
 
 @Component({
   selector: 'app-mapa',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private localizacion: GeolocalizacionService) { }
 
   ngOnInit(): void {
+    this.getLocation();
   }
+
+  latitud: string='';
+  longitud : string='';
+
+  getLocation() {
+    this.localizacion.getPosition().then(pos => {
+        this.latitud = pos.lat;
+        this.longitud = pos.lng;
+    });
+}
 
 }
---
+++ src/app/service/geolocalizacion/geolocalizacion.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { EventEmitter, Injectable, Output } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GeolocalizacionService {
+
+  constructor() { }
+
+
+
+  getPosition(): Promise<any> {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resp => {
+                resolve({lng: resp.coords.longitude, lat: resp.coords.latitude});
+            },
+            err => {
+                reject(err);
+          });
+    });
+}
+
+}
